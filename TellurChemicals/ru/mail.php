ad9b5015a9e43e6db3c5766e9010e419febd1465
kdvprog <133968650+kdvprog@users.noreply.github.com>
2023-07-27 21:47:08 +0300

    Add files via upload

--- TellurChemicals/ru/mail.php
+++ TellurChemicals/ru/mail.php
@@ -1,3 +1,8 @@
 <?php
-  echo "Информация принята"
+  $name = $_POST['name'];
+  $phone = $_POST['phone'];
+  $email = $_POST['email'];
+  $text = $_POST['text'];
+  echo $name;
+  echo "Информация принята";
 ?>
